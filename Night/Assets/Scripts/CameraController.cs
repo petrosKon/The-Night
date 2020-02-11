89bc5d7a05c4ffb9e70e117e856c57f53b23d52d
petros007 <manrock7788@gmail.com>
2020-02-11 18:02:45 +0200

    Camera Movement Updated

--- Night/Assets/Scripts/CameraController.cs
+++ Night/Assets/Scripts/CameraController.cs
@@ -15,6 +15,11 @@ public class CameraController : MonoBehaviour
     //The point where the camera is going to turn around
     public Transform pivot;
 
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    public bool invertY;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,7 +43,26 @@ public class CameraController : MonoBehaviour
 
         //Get the Y position of the mouse & rotate the pivot
         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        pivot.Rotate(-vertical,0,0);
+        // pivot.Rotate(-vertical,0,0);
+        if (invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+
+        }
+        //Limit up/down camera rotation
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0,0);
+        }
+
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
 
         //Move the camera based on the current rotation of the target & the original offset
         float desiredYAngle = target.eulerAngles.y;
