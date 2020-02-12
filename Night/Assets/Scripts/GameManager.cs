9669de59514fd48df31574b520e7fe116c6bf3d8
petros007 <manrock7788@gmail.com>
2020-02-12 13:53:30 +0200

    Pick up items added

---
+++ Night/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int currentPoints;
+    public TextMeshProUGUI pointsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddPoints(int pointsToAdd)
+    {
+        currentPoints += pointsToAdd;
+        pointsText.text = "Points: " + currentPoints.ToString();
+
+    }
+}
---
+++ Night/Assets/Scripts/PointsPickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsPickup : MonoBehaviour
+{ 
+    public int value;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().AddPoints(value);
+
+            Destroy(gameObject);
+        }
+    }
+}
