a1f05125d9e641066a1b1fa6954c802cb79f8f98
petros007 <manrock7788@gmail.com>
2020-02-26 16:18:33 +0200

    Added Magnet Prefab

---
+++ Night/Assets/Scripts/MagnetPowerUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetPowerUp : MonoBehaviour
+{
+    public float magnetSize = 10f;
+    public float magnetDuration = 5f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            
+        }
+    }
+}
